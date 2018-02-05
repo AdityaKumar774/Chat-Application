4337e07a0d787fed4ff79de168fd1ccef21d7c68
Aditya Kumar <aditya.kumar774@gmail.com>
2018-02-06 01:33:11 +0530

    New Modifications

---
+++ includes/database/connect.db.php
@@ -0,0 +1,18 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+
+$db_name = 'chat';
+
+if($connection = mysqli_connect($db_host, $db_user, $db_pass)){
+    echo 'Connected to Database Server... <br />';
+    if ($database = mysqli_select_db($db_name, $connection)){
+        echo 'Database has been selected... <br />';
+    }else {
+        echo 'Database was not found... <br />';
+    }
+}else {
+    echo 'Unable to connect to MYSQL Server <br />';
+}
+?>
--- logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con  = mysqli_connect('localhost', 'root', '');
-mysqli_select_db($con, 'chatbox');
-$result1 = mysqli_query($con, "SELECT * FROM logs ORDER BY id DESC");
-while($extract = mysqli_fetch_array($result1)){
-    echo "<span class='uname'>" . $extract['username'] . "</span>: <span class='msg'>" . $extract['msg'] . "</span><br />";
-}
-?>
